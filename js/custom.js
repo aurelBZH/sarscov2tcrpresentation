38f520f12ef18a0f7b8dd4b4f8bd0f5a139cd9f4
aurel <aurelien.beliard@laposte.net>
2019-11-04 01:14:13 +0100

    modif

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,9 @@
 function showImage() {
     var image = document.querySelector(".myImg");
-    if (image.style["display"] = "block"){
+    if (image.style["display"] == "block"){
         console.log(image.style["display"]+"ihihih")
         image.style["display"] = "none";}
-    else if (image.style["display"] = "none"){
+    else if (image.style["display"] == "none"){
         console.log(image.style["display"]+"ahahahah")
         image.style["display"] = "block";
     }
